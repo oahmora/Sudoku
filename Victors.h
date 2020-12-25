36152631bf1dab11fbedf0ff16b9f941f04095cc
ohern017 <ohern017@ucr.edu>
2020-12-25 00:39:51 -0800

    Added files from deleted repository.

---
+++ Victors.h
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Victors.h
+ * Author: User
+ *
+ * Created on December 10, 2016, 8:54 PM
+ */
+
+#ifndef VICTORS_H
+#define VICTORS_H
+
+//const short NMESIZE=20;
+
+/*struct Difs{
+    short veasy=0;
+    short easy=0;
+    short normal=0;
+    short hard=0;
+    short vhard=0;
+};*/
+
+struct Victors{
+    string name;
+    short score;
+    //Difs dif;
+};
+
+#endif /* ARRAY_H */
+
